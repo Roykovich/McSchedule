8fad1e380bdd1a196bc0731a14346adb3123cabb
Roykovich <>
2024-09-22 04:30:24 -0300

    add get_current_week_schedule feature

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template
-from services.schedule import get_schedule
+from services.schedule import get_schedule, get_clock_ins
+from services.get_weeks import get_current_week, get_current_week_schedule
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        current_week_schedule=get_current_week_schedule()
+    )
 
 @app.route('/schedule')
 def schedule():
     schedule = get_schedule()
 
     return schedule
+
+@app.route('/clock_ins')
+def clock_ins():
+    clock_ins = get_clock_ins()
+
+    return clock_ins
